6397e270014d717fddc80a0fa0a35fc824fe79c6
Adrian Hill <adrian.hill@mailbox.org>
2024-03-05 15:38:09 +0100

    Test code formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,17 @@ using ImageNetDataset
 using Test
 using Aqua
 using JET
+using JuliaFormatter
 
 @testset "ImageNetDataset.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(ImageNetDataset)
+        Aqua.test_all(ImageNetDataset; ambiguities=false)
     end
     @testset "Code linting (JET.jl)" begin
         JET.test_package(ImageNetDataset; target_defined_modules=true)
+    end
+    @testset "Code formatting (JuliaFormatter.jl)" begin
+        @test format(ImageNetDataset; verbose=false, overwrite=false)
     end
     # Write your tests here.
 end
