20fd40c402a617fd1ef951ae56727aa9a2a91c69
Documenter.jl <documenter@juliadocs.github.io>
2024-03-05 14:54:15 +0000

    build based on 8377b8e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ImageNetDataset","category":"page"},{"location":"#ImageNetDataset","page":"Home","title":"ImageNetDataset","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ImageNetDataset.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ImageNetDataset]","category":"page"}]
+}
